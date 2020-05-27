2f1613a945aa0d9a440db44b858dcd00da454d7f
haotianmichael <2398419426@buaa.edu.cn>
2020-05-27 22:56:15 +0800

    add Clang-Fronted-IRGenerator
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

---
+++ clang/libclang/irgen.cpp
@@ -0,0 +1,29 @@
+#include "/LLVM/llvm-install/include/llvm/ADT/IntrusiveRefCntPtr.h"
+#include "/LLVM/llvm-install/include/llvm/Support/CommandLine.h"
+#include "/LLVM/llvm-install/include/clang/AST/ASTContext.h"
+#include "/LLVM/llvm-install/include/clang/AST/ASTConsumer.h"
+#include "/LLVM/llvm-install/include/clang/Basic/Diagnostic.h"
+#include "/LLVM/llvm-install/include/clang/Basic/DiagnosticOptions.h"
+#include "/LLVM/llvm-install/include/clang/Basic/FileManager.h"
+#include "/LLVM/llvm-install/include/clang/Basic/SourceManager.h"
+#include "/LLVM/llvm-install/include/clang/Basic/LangOptions.h"
+#include "/LLVM/llvm-install/include/clang/Basic/TargetInfo.h"
+#include "/LLVM/llvm-install/include/clang/Basic/TargetOptions.h"
+#include "/LLVM/llvm-install/include/clang/Frontend/ASTConsumers.h"
+#include "/LLVM/llvm-install/include/clang/Frontend/CompilerInstance.h"
+#include "/LLVM/llvm-install/include/clang/Frontend/TextDiagnosticPrinter.h"
+#include "/LLVM/llvm-install/include/clang/Lex/Preprocessor.h"
+#include "/LLVM/llvm-install/include/clang/Parse/Parser.h"
+#include "/LLVM/llvm-install/include/clang/Parse/ParseAST.h"
+#include <iostream>
+
+
+using namespace llvm;
+using namespace clang;
+
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
