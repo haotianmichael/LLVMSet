527d848323d1ebe7fdc7c3046964c4585f054d90
haotianmichael <2398419426@buaa.edu.cn>
2020-07-08 08:08:16 +0800

    add ExprAST and its subclass for kaleidoscope
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- Kaleidoscope/parser.cpp
+++ Kaleidoscope/parser.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <vector>
 
-using namespace std;
 
 
 enum Token{
@@ -54,7 +53,7 @@ static int gettok() {
             NumStr += LastChar;
             LastChar = getchar(); 
         }while(isdigit(LastChar) || LastChar == '.'); 
-    
+
         NumStr = strtod(NumStr.c_str(), nullptr); 
         return tok_number;
     }
@@ -62,9 +61,9 @@ static int gettok() {
 
     if(LastChar == '#') {        //Comment until end of line
         do 
-           LastChar = getchar();
+            LastChar = getchar();
         while(LastChar != EOF && LastChar != '\n' && LastChar != '\r'); 
-    
+
         if(LastChar != EOF)
             return gettok();
     }
@@ -80,4 +79,91 @@ static int gettok() {
     return ThisChar;
 
 }
+
+namespace {
+
+    // ExprAST - Base class for all expression nodes.
+    class ExprAST
+    {
+        public:
+            virtual ~ExprAST () = default;
+    };
+
+
+    // NumberExprAST - Expression class for numeric literals like "1.0".
+    class NumberExprAST : public ExprAST
+    {
+        double Val;
+        public:
+        NumberExprAST(double Val) : Val(Val) {} 
+    };
+
+
+
+    //VariableExprAST - Expression class for referencing a variable, like "a".
+    class VariableExprAST  : public ExprAST
+    {
+        std::string Name;
+    public:
+        VariableExprAST (const std::string &Name) : Name(Name){}
+    };
+
+
+    // BinaryExprAST - Expression class for a binnary operator.
+    class BinaryExprAST: public ExprAST
+    {
+        char Op;
+        std::unique_ptr<ExprAST> LHS, RHS;
+    public:
+        BinaryExprAST (char Op, std::unique_ptr<ExprAST> LHS, 
+                std::unique_ptr<ExprAST> RHS)
+            : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+    };
+    
+
+    // CallExprAST - Expression class for function calls.
+    class CallExprAST : public ExprAST
+    {
+        std::string Callee;
+        std::vector<std::unique_ptr<ExprAST>> Args;
+    public:
+        CallExprAST (const std::string &Callee, 
+                std::vector<std::unique_copy<ExprAST>> Args)
+            : Callee(Callee), Args(std::move(Args)) {}
+    
+    };
+
+
+    // PrototypeAST - This class represents the "prototype" for a function,
+    // which captrues its name, and its argument names(thus implicitly the number 
+    // of arguments the function takes)
+    class PrototypeAST
+    {
+        std::string name;
+        std::vector<std::string> Args;
+    public:
+        PrototypeAST (const std::string &Name, std::vector<std::string> Args)
+            : Name(Name), Args(std::move(Args)) {}
+    
+        const std::string &getName() const {return name; } 
+    };     
+
+
+    // FunctionAST - This class represents a function definition itself.
+    class FunctionAST
+    {
+        std::unique_ptr<PrototypeAST> Proto;
+        std::unique_ptr<ExprAST> Body;
+    public:
+        FunctionAST (std::unique_ptr<PrototypeAST> Proto,
+                std::unique_ptr<ExprAST> Body)
+            : Proto(std::move(Proto)), Body(std::move(Body)) {}
+        
+    };
+
+
+}
+
+
+
 
